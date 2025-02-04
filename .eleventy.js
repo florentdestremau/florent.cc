6ace9d0a9bbde94bc6ea5c373a5a2128c512678e
florentdestremau <florentdestremau@users.noreply.github.com>
2025-02-04 10:41:10 +0000

    deploy: 9b4ae42203d4518897760490bf5a79d0d06a00ec

--- .eleventy.js
+++
@@ -1,7 +0,0 @@
-// filepath: /.eleventy.js
-module.exports = function (eleventyConfig) {
-    // Passthrough copy for CSS files
-    eleventyConfig.addPassthroughCopy("css");
-    return {
-    };
-};
--- README.md
+++
@@ -1,1 +0,0 @@
-# Florent Destremau
